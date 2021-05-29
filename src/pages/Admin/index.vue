de46b5ea814bd6871e198d3ef9f6d7e376803c8b
Renzo Cayanan <cayanan.chavez@gmail.com>
2021-05-29 22:18:35 +0800

    admin update registration

--- src/pages/Admin/index.vue
+++ src/pages/Admin/index.vue
@@ -1,7 +1,36 @@
 <template>
-  <div class="q-pa-md q-gutter-lg">
-
+  <div class="q-pa-md q-gutter-md">
     <!-- <q-btn color="primary" label="Primary" @click="getUserData" /> -->
+       <q-btn class="row align-center" :disable="started" @click="oddedit = true" color="white" text-color="black" label="Register" icon="edit" align="right" />
+      <div id="oddss" class="row q-gutter-xs text-center col">
+        <q-dialog v-model="oddedit" persistent class="col">
+            <q-card>
+                <q-card-section class="col-4 items-right q-gutter-md">
+                <q-input class="" type="text" outlined color="black" v-model="fullname" label="Full Name" />
+                <q-input class="" type="text" outlined color="black" v-model="address" label="Address" />
+                <q-input class="" type="text" outlined color="black" v-model="contactnumber" label="Contact Number" />
+                <q-radio v-model="role" val="cashier" label="Cashier" color="red" />
+                <q-radio v-model="role" val="admin" label="Admin" color="blue" />
+                </q-card-section>
+
+                <q-card-actions align="right">
+                <q-btn :disable="savestart" @click="save = true" label="Save" color="green" v-close-popup />
+                <q-btn label="Cancel" color="primary" v-close-popup />
+        <q-dialog v-model="save" persistent class="col-4">
+            <q-card>
+                <q-card-section class="col q-gutter-md">
+                 <q-label >Successfully Saved!</q-label>
+                <!-- <q-label v-model="fullname">{{fullname}}</q-label>
+                <q-label v-model="address">{{address}}</q-label>
+                <q-label v-model="contactnumber">{{contactnumber}}</q-label> -->
+                <q-btn class="col" size="sm" align="center" label="Okay" color="primary" v-close-popup />
+                </q-card-section>
+            </q-card>
+        </q-dialog>
+                </q-card-actions>
+            </q-card>
+        </q-dialog>
+    </div>
     <div id="history">
         <q-table
             class="my-sticky-column-table"
@@ -13,10 +42,8 @@
     </div>
   </div>
 </template>
-
 <script>
 import { axiosCont } from 'boot/axios'
-
 export default {
   data () {
     return {
@@ -26,8 +53,11 @@ export default {
       winnerPop: false,
       started: false,
       oddedit: false,
-      meron: 100,
-      wala: 10,
+      savestart: false,
+      save: false,
+      fullname: '',
+      address: '',
+      role: '',
       winner: '',
       columns: [
         {
