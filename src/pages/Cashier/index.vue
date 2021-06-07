6bf524d82fa028c89cf1659cea5dab3c8dcb54f6
bree gemina <breealinghawa@gmail.com>
2021-06-08 01:00:11 +0800

    initial design for cashier

--- src/pages/Cashier/index.vue
+++ src/pages/Cashier/index.vue
@@ -1,100 +1,41 @@
 <template>
-  <div class="q-pa-md q-gutter-lg">
-    <div id="match" class="row q-gutter-xs text-center">
-        <q-card dark bordered class="col text-black bg-white my-card">
-            <q-card-section>
-                <div class="text-h3">Match #</div>
-            </q-card-section>
-
-            <q-separator dark inset />
-
-            <q-card-section>
-                <div class="text-h1 text-red">{{ current_data === null ? 'NO MATCHES' : current_data.match_number }}</div>
-            </q-card-section>
-        </q-card>
-    </div>
-    <div id="oddss"  class="row q-gutter-xs text-center">
-        <div class="col q-gutter-sm" style="padding-right: 1px">
-            <q-card dark bordered class="bg-red-7 my-card">
+    <div class="q-pa-lg text-white">
+        <div class="row">
+            <div class="col-6 text-center">
+              <q-card dark bordered class="col text-black bg-white my-card">
                 <q-card-section>
-                    <div class="text-h2" style="padding-top: 10px">MERON</div>
+                    <div class="text-h4">Match #</div>
                 </q-card-section>
-
-                <q-separator dark inset />
-
-                <q-card-section>
-                    <div class="text-h3"> {{ current_data === null ? 'NO MATCHES' : current_data.meron_odd }} </div>
+                <q-separator inset />
+                <q-card-section class="q-pa-md text-h3">
+                    {{ current_data === null ? 'NO MATCHES' : current_data.match_number }}
                 </q-card-section>
-            </q-card>
-            <q-btn size="xl" style="width: 98.5%;" color="red" @click="chooseSide('MERON')" push label="MERON" />
-        </div>
+              </q-card>
 
-        <div class="col q-gutter-sm" style="padding-left: 1px;">
-            <q-card dark bordered class="bg-blue-7 my-card">
-                <q-card-section>
-                    <div class="text-h2" style="padding-top: 10px">WALA</div>
-                </q-card-section>
+               <q-card class="q-mt-md" style="background: radial-gradient(circle, rgb(255, 163, 163) 0%, rgb(184, 0, 6) 100%)">
+                  <q-card-section>
+                    <div class="text-h4">MERON</div>
+                  </q-card-section>
+                  <q-separator dark inset />
+                  <q-card-section class="q-pa-md text-h3">
+                   120
+                  </q-card-section>
+                </q-card>
 
-                <q-separator dark inset />
+               <q-card class="q-mt-md" style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)">
+                  <q-card-section>
+                    <div class="text-h4">WALA</div>
+                  </q-card-section>
+                  <q-separator dark inset />
+                  <q-card-section class="q-pa-md text-h3">
+                   100
+                  </q-card-section>
+                </q-card>
+            </div>
+            <div class="col-6">
 
-                <q-card-section>
-                    <div class="text-h3"> {{ current_data === null ? 'NO MATCHES' : current_data.wala_odd }} </div>
-                </q-card-section>
-            </q-card>
-            <q-btn size="xl" style="width: 98.5%" color="blue" push label="WALA"  @click="chooseSide('WALA')"/>
+            </div>
         </div>
-    </div>
-
-   <q-card style="margin-left: auto; margin-right: auto; margin-top: 70px; max-width: 999px; width: 100%">
-       <q-card-section class="row q-gutter-md">
-        <div class="col-5 q-gutter-md">
-          <!-- <q-input outlined v-model="nickname" label="Nickname" /> -->
-          <q-input :disable="disabe_betting" outlined v-model="betamount" label="Bet">
-            <template v-slot:before>
-              <q-icon name="person"/>
-            </template>
-          </q-input>
-            <q-card-actions align="right">
-             <q-btn :disable="disabe_betting" class="full-width" icon="monetization_on" @click="createBet" label="Bet" color="green" v-close-popup />
-           </q-card-actions>
-        </div>
-        <div class="q-pa-md col">
-          <q-list bordered separator>
-              <template>
-                <div class="card">
-                  <div class="card-title bg-primary text-white">
-                    <q-item class="column">
-                      <q-item-label align="left" v-model="sultada" class="text-h6" style="margin-top: 10px">Sultada # 24</q-item-label>
-                      <q-item-label align="left" v-model="sultada" class="text-subtitle2" style="margin-bottom: 10px"> May 20, 2021  1:03.00 PM </q-item-label>
-                    </q-item>
-                  </div>
-                </div>
-              </template>
-            <q-item style="padding-top: 20px; margin-left: 6px">
-                <q-item-label>Bet Side <b :class="bet_color">{{ betside }}</b></q-item-label>
-            </q-item>
-            <q-item style="padding-top: 20px; margin-left: 6px">
-              <q-item-label> Odds: {{ odds }}</q-item-label>
-            </q-item>
-            <q-item style="padding-top: 20px; margin-left: 6px">
-                <q-item-label v-model="betamount">Bet Amount: <strong>{{ betamount }}</strong></q-item-label>
-            </q-item>
-              <q-item style="padding-top: 20px; margin-left: 6px">
-              <q-item-label>Bet Prize: {{ bet_prize }}</q-item-label>
-            </q-item>
-            <q-item >
-                <q-item-label>Total Payout: <strong>{{ computedBet }}</strong></q-item-label>
-            </q-item>
-              <q-card-actions align="left" style="padding-bottom: 25px; padding-top: 25px; margin-left: 10px">
-                <q-btn-group push>
-                  <q-btn color="green" glossy text-color="white" push label="Print" icon="print" style="padding-right: 5px"></q-btn>
-                  <q-btn color="primary" glossy text-color="white" push label="Back" style="padding-right: 10px; padding-left: 10px"></q-btn>
-                </q-btn-group>
-              </q-card-actions>
-          </q-list>
-        </div>
-          </q-card-section>
-      </q-card>
     </div>
 </template>
 
