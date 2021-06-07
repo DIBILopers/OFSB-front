57cf3b1282abd4c6ce59c5607219c37d711ae8ed
kurokidejiga143112 <givereedejiga143112@gmail.com>
2021-06-08 00:37:58 +0800

    changes

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const axiosCont = axios.create({
-  baseURL: 'http://localhost:8000/'
+  // baseURL: 'http://localhost:8000/'
+  baseURL: 'http://192.168.0.26:8000'
+  // headers: {"Access-Control-Allow-Origin": "*"}
 })
 
 export { axiosCont }
